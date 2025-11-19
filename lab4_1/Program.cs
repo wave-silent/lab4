322b447b19d9275e8958caac0b5f01bb8cc0287b
wave-silent <danillogutov.74@mail.ru>
2025-11-19 15:09:35 +0500

    Update Program.cs

--- lab4_1/Program.cs
+++ lab4_1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,7 +12,7 @@ namespace lab4
         {
             Validator validator = new Validator();
             int num1;
-            Console.Write("Введите номер задания из списка \"1 2 3 4 5 6 7\": ");
+            Console.Write("Введите номер задания из списка \"1 2 3 4 5\": ");
             num1 = Convert.ToInt32(validator.check_int(Console.ReadLine()));
             switch (num1)
             {
@@ -21,11 +21,11 @@ namespace lab4
                         int n1, n2;
                         Console.Write("Введите количество элементов в списке L1: ");
                         n1 = Convert.ToInt32(validator.check_int(Console.ReadLine()));
-                        List<string> L1 = Collections.CreateList(n1);
+                        List<string> L1 = Collections.CreateList<string>(n1);
                         Console.WriteLine();
                         Console.Write("Введите количество элементов в списке L2: ");
                         n2 = Convert.ToInt32(validator.check_int(Console.ReadLine()));
-                        List<string> L2 = Collections.CreateList(n2);
+                        List<string> L2 = Collections.CreateList<string>(n2);
 
                         List<string> L = Collections.Intersect(L1, L2);
 
@@ -39,13 +39,13 @@ namespace lab4
                         int n1;
                         Console.Write("Введите количество элементов в списке L: ");
                         n1 = Convert.ToInt32(validator.check_int(Console.ReadLine()));
-                        List<string> L = Collections.CreateList(n1);
+                        List<string> L = Collections.CreateList<string>(n1);
 
-                        List<string> result = Collections.LinkedList(L);
+                        //List<object> result = Collections.LinkedList(L);
 
-                        Console.WriteLine();
-                        Console.Write("Итоговый результат: ");
-                        Console.WriteLine(Collections.PrintList(result));
+                        //Console.WriteLine();
+                        //Console.Write("Итоговый результат: ");
+                        //Console.WriteLine(Collections.PrintList(result));
                         break;
                     }
                 case 3:
@@ -68,7 +68,15 @@ namespace lab4
                         n4 = Convert.ToInt32(validator.check_int(Console.ReadLine()));
                         HashSet<string> SetSt4 = Collections.CreateSetStudent(n4);
                         Console.WriteLine();
-                   
+                        Dictionary<string, HashSet<string>> st = new Dictionary<string, HashSet<string>>();
+
+                        st.Add("Студент 1", SetSt1);
+                        st.Add("Студент 2", SetSt2);
+                        st.Add("Студент 3", SetSt3);
+                        st.Add("Студент 4", SetSt4);
+
+                        HashSet<string> GeneralHashSet = new HashSet<string> { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
+
                         Console.Write("Какой пункт из трех хотите решить: ");
                         num = Convert.ToInt32(validator.check_int(Console.ReadLine()));
                         switch (num)
@@ -76,46 +84,23 @@ namespace lab4
                             // Решение 1 пункта
                             case 1:
                                 {
-                                    SetSt1.IntersectWith(SetSt2);
-                                    SetSt1.IntersectWith(SetSt3);
-                                    SetSt1.IntersectWith(SetSt4);
+                                    HashSet<string> res = Collections.task5_1(st, GeneralHashSet);
                                     Console.Write("Перечень ТРЦ, в которые ходили все студенты группы: ");
-                                    Console.WriteLine(Collections.PrintHashSet(SetSt1));
+                                    Console.WriteLine(Collections.PrintHashSet(res));
                                     break;
                                 }
                             // Решение 2 пункта
                             case 2:
                                 {
-                                    HashSet<string> allStudent = new HashSet<string>(SetSt1);
-                                    allStudent.UnionWith(SetSt2);
-                                    allStudent.UnionWith(SetSt3);
-                                    allStudent.UnionWith(SetSt4);
-
-                                    HashSet<string> intersectStudent = new HashSet<string>(SetSt1);
-                                    intersectStudent.IntersectWith(SetSt2);
-                                    intersectStudent.IntersectWith(SetSt3);
-                                    intersectStudent.IntersectWith(SetSt4);
-
-                                    HashSet<string> someStudent = new HashSet<string>(allStudent);
-                                    someStudent.ExceptWith(intersectStudent);
-
+                                    HashSet<string> res = Collections.task5_2(st, GeneralHashSet);
                                     Console.Write("Перечень ТРЦ, в которые ходили некоторые студенты группы: ");
-                                    Console.WriteLine(Collections.PrintHashSet(someStudent));
+                                    Console.WriteLine(Collections.PrintHashSet(res));
                                     break;
                                 }
                             // Решение 3 пункта
                             case 3:
                                 {
-                                    // Общий перечень центров 
-                                    HashSet<string> GeneralHashSet = new HashSet<string> { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
-
-                                    HashSet<string> allStudent = new HashSet<string>(SetSt1);
-                                    allStudent.UnionWith(SetSt2);
-                                    allStudent.UnionWith(SetSt3);
-                                    allStudent.UnionWith(SetSt4);
-
-                                    GeneralHashSet.ExceptWith(allStudent);
-
+                                    HashSet<string> res = Collections.task5_3(st, GeneralHashSet);
                                     Console.Write("Перечень ТРЦ, в которые не ходили никакие студенты группы: ");
                                     Console.WriteLine(Collections.PrintHashSet(GeneralHashSet));
                                     break;
@@ -126,7 +111,7 @@ namespace lab4
                 case 4:
                     {
                         Console.Write("Количество разных букв, которые встречаются в тексте: ");
-                        Console.WriteLine( Collections.CountDifferentLetter());
+                        Console.WriteLine(Collections.CountDifferentLetter());
                         break;
                     }
                 case 5:
@@ -135,59 +120,8 @@ namespace lab4
                         Console.WriteLine(Collections.DictionaryOfAthletes());
                         break;
                     }
-                case 6:
-                    {
-                        Console.WriteLine("Введите x и y для 1 отрезка: ");
-                        LineSegment line1 = new LineSegment(Convert.ToDouble(validator.check_double(Console.ReadLine())), Convert.ToDouble(validator.check_double(Console.ReadLine())));
-                        Console.WriteLine();
-                        Console.WriteLine("Введите x и y для 2 отрезка: ");
-                        LineSegment line2 = new LineSegment(Convert.ToDouble(validator.check_double(Console.ReadLine())), Convert.ToDouble(validator.check_double(Console.ReadLine())));
-
-                        Console.WriteLine(line1);
-                        Console.Write("Итоговый результат: ");
-                        Console.WriteLine(line1.IntersectionOfSegments(line2));
-                        break;
-                    }
-                case 7:
-                    {
-                        Console.WriteLine("Введите x и y для 1 отрезка: ");
-                        LineSegment line1 = new LineSegment(Convert.ToDouble(validator.check_double(Console.ReadLine())), Convert.ToDouble(validator.check_double(Console.ReadLine())));
-                        Console.WriteLine();
-                        Console.WriteLine("Введите x и y для 2 отрезка: ");
-                        LineSegment line2 = new LineSegment(Convert.ToDouble(validator.check_double(Console.ReadLine())), Convert.ToDouble(validator.check_double(Console.ReadLine())));
-
-                        double range = !line1;
-                        Console.WriteLine("Длина диапазона: " + range);
-                        Console.WriteLine();
-
-                        //line1 = line1++;
-                        //Console.WriteLine("Расширенный на 1 отрезок: " + line1);
-                        //Console.WriteLine();
-
-                        int x = (int)line1;
-                        Console.WriteLine("Целая часть координаты x: " + x);
-                        Console.WriteLine();
-
-                        double y = (double)line1;
-                        Console.WriteLine("Координата y: " + y);
-                        Console.WriteLine();
-
-                        //line1 = line1 - 10;
-                        //Console.WriteLine("Уменьшение координаты x: " + line1);
-                        //Console.WriteLine();
-
-                        //line1 = 10 - line1;
-                        //Console.WriteLine("Уменьшение координаты y: " + line1);
-                        //Console.WriteLine();
-
-                        //Для пересечения отрезков надо было закомменитровать другие некоторые методы 2, 5, 6
-                        bool IntersectLine = line1 < line2;
-                        Console.WriteLine("Пересекаются ли отрезки: " + IntersectLine);
-                        Console.WriteLine();
-                        break;
-                    }
             }
+            
         }
     }
 }
-
